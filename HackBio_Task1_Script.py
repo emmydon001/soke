24bcc2ebd3663fd8deea16c3d8ed5aa5546f86ce
emmydon001 <69091298+emmydon001@users.noreply.github.com>
2020-08-03 22:37:30 +0100

    Add files via upload

---
+++ HackBio_Task1_Script.py
@@ -0,0 +1,10 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:\Users\Emmanuel Sokefun\Desktop\HackBio_Task1_Script.py ======
+Name: Owolabi Emmanuel Sokefun
+Email: eaibukunoluwa@gmail.com
+Language: Python 3.85
+Biostack: Vaccine Informatics
+Slack name: @Emmy
+>>> 
